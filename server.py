9d8aa8e2194d3e98a022dc51e637d81a85c4485c
Василий <dfvasia@gmail.com>
2022-06-29 17:58:25 +0300

    end

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
+from typing import Dict, Type
+
 from flask import Flask, request, render_template, redirect, url_for
 
 from game.Models.equipment_models import EquipmentData
+from game.Models.personage_models import Hero
+from game.hero import Player
 from game.personages import personage_class
 from game.utils import load_equipment
 
-equipment: EquipmentData = load_equipment()
+EQUIPMENT: EquipmentData = load_equipment()
 
-print(equipment)
+heroes: Dict[str, Hero] = dict()
 
 
 def render_choose_personage_template(**kwargs) -> str:
     return render_template(
         'hero_choosing.html',
         classes=personage_class.values(),
-        equipment=equipment,
+        equipment=EQUIPMENT,
         **kwargs,
     )
 
@@ -33,6 +37,12 @@ def create_app(config_obj):
                 header='Выберите героя',
                 next_button='Выбрать врага',
             )
+        heroes['player'] = Player(
+            class_=personage_class[request.form['unit_class']],
+            weapon=EQUIPMENT.get_weapon(request.form['weapon']),
+            armor=EQUIPMENT.get_armor(request.form['armor']),
+            name=request.form['name']
+        )
         return redirect(url_for('choose-enemy'))
 
     @app.route('/choose-enemy', methods=['GET', 'POST'])
@@ -42,5 +52,17 @@ def create_app(config_obj):
                 header='Выберите врага',
                 next_button='Начать сражение'
             )
+        heroes['enemy'] = Player(
+            class_=personage_class[request.form['unit_class']],
+            weapon=EQUIPMENT.get_weapon(request.form['weapon']),
+            armor=EQUIPMENT.get_armor(request.form['armor']),
+            name=request.form['name']
+        )
         return '<h2>Not implemented</h2>'
+
+    @app.route('/fight')
+    def start_fight():
+        if 'player' in heroes and 'enemy' in heroes:
+            return render_template('fight.html', heroes=heroes, results='Fight!')
+        return redirect(url_for('index'))
     return app
